f2c155714948de70b150e4ba64d0d4905fde33b3
David Alexander <david@campusspecial.com>
2014-02-26 12:37:15 -0500

    Retrieve ticket comments from JSON endpoint

--- issue_comments.rb
+++ issue_comments.rb
@@ -4,43 +4,44 @@ require 'logger'
 
 
 class SLSCrawler
-	def init:
+    attr_writer :password
+    attr_accessor :username, :organization, :agent
+
+	def initialize
 		@agent = Mechanize.new{ |a| a.ssl_version, a.verify_mode = 'SSLv3', OpenSSL::SSL::VERIFY_NONE }
 		@agent.log = Logger.new 'mech.log'
 		@agent.user_agent_alias = 'Mac Safari'
 	end
 
-	def username:
-		@username
-	end
-	def username=(username):
-		@username = username
-	end
-	def password:
-		@password
-	end
-	def password=(password):
-		@password = password
-	end
-	def organization:
-		@organization
-	end
-	def organization=(organization):
-		@organization = organization
-	end
+    def base_url
+        "https://#{@organization}.springloops.io"
+    end
 
-	def login:
-		login_form = @agent.get("https://#{@organization}.springloops.io/login").forms[0]
+	def login
+        page = @agent.get(self.base_url + "/login")
+        unless page.link_with(:href => '/logout').nil?
+            return true
+        end
+        login_form = page.forms[0]
 		login_form.login = @username
-		login_form.login = @password
+		login_form.password = @password
 
 		results = @agent.submit login_form
 
-		unless results.link_with :href => '/login'
+		unless results.link_with :href => '/logout'
 			return false
 		else
 			return true
 		end
 	end
+
+    def get_comments project_id, ticket_id
+        params = {
+            'ticketId' => '0',
+            'projectId' => project_id.to_s,
+            'relativeId' => ticket_id.to_s
+        }
+        JSON.parse(@agent.get(self.base_url + "/ajax/fetch-ticket.html", params))
+    end
 end
 
