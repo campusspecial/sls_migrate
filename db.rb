acd990bff3e33d388d9757534bf9bd3edc43d028
David Alexander <david@campusspecial.com>
2014-02-28 17:47:22 -0500

    Restructuring ActiveRecord models

--- db.rb
+++ db.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-# vim:tabstop=2:shiftwidth=2:expandtab
+# vim: ts=2:tw=2:sw=2:expandtab
 
 require 'rubygems'
 require 'active_record'
@@ -20,6 +20,22 @@ ActiveRecord::Schema.define do
     end
   end
 
+  unless ActiveRecord::Base.connection.tables.include? 'projects'
+    create_table :projects do |table|
+      table.column :name, :string
+      table.column :sls_id, :integer
+      table.column :github_id, :integer
+    end
+  end
+
+  unless ActiveRecord::Base.connection.tables.include? 'projectassignments'
+    create_table :projectassignments do |table|
+      # many-to-many facilitator
+      table.column :project_id, :integer
+      table.column :user_id, :integer
+    end
+  end
+
   unless ActiveRecord::Base.conneciton.tables.include? 'tickets'
     create_table :tickets do |table|
       table.column :title, :string
@@ -36,15 +52,20 @@ ActiveRecord::Schema.define do
     end
   end
 
-  unless ActiveRecord::Base.connection.tables.include? 'comments'
-    create_table :comments do |table|
-      table.column :comment, :text
-      table.column :sls_id, :integer
-      table.column :github_id, :integer
-      table.column :created, :timestamp
+  unless ActiveRecord::Base.connection.tables.include? 'ticketassignments'
+    create_table :ticketassignments do |table|
+      # many-to-many facilitator
+      table.column :user, :integer
+      table.column :ticket, :integer
+    end
+  end
 
-      # Foreign Keys
-      table.column :owner, :integer
+  unless ActiveRecord::Base.connection.tables.include? 'labels'
+    create_table :labels do |table|
+      table.column :name, :string
+      
+      # Foreign keys
+      table.column :project_id, :integer
     end
   end
 
@@ -55,21 +76,152 @@ ActiveRecord::Schema.define do
       table.column :ticket, :integer
     end
   end
+
+  unless ActiveRecord::Base.connection.tables.include? 'ticketevents'
+    create_table :ticketevents do |table|
+      table.column :created, :timestamp
+      table.column :table, :string
+      table.column :table_id, :integer
+
+      # Foreign keys
+      table.column :ticket_id, :integer
+    end
+  end
+
+  unless ActiveRecord::Base.connection.tables.include? 'comments'
+    create_table :comments do |table|
+      table.column :comment, :text
+      table.column :sls_id, :integer
+      table.column :github_id, :integer
+      table.column :created, :timestamp
+
+      # Foreign keys
+      table.column :owner, :integer # user account
+      table.column :ticketevent_id, :integer
+    end
+  end
+
+  unless ActiveRecord::Base.connection.tables.include? 'statuschanges'
+    create_table :statuschanges do |table|
+      table.column :name, :string
+      table.column :is_open, :boolean
+      table.column :created, :timestamp
+
+      # Foreign keys
+      table.column :ticketevent_id, :integer
+    end
+  end
 end
 
-class SLSUser
-  include DataMapper::Resource
-  
-  property :id, Serial
-  property :f_name, String
-  property :l_name, String
-  property :email, String
-  property :short_name, String
-  property :sls_id, Integer, :key => true
-  property :github_id, Integer, :key => true
+class User < ActiveRecord::Base
+  has_many :creations, :class_name => 'Ticket'
+  has_many :assignments, :class_name => 'Ticket', :through => :ticket_assignments
+  has_many :projects, :through => :project_assignments
+  has_many :comments
+  has_many :activities
 
-  has n, :tickets, 'SLSTicket'
+  def recent_activities(limit)
+    activities.order('created_at DESC').limit(limit)
+  end
 end
+
+class Project < ActiveRecord::Base
+  has_many :users, :through => :project_assigments
+  has_many :activities, :through => :tickets
+end
+
+class Ticket < ActiveRecord::Base
+  belongs_to :project
+  belongs_to :creator, :class_name => 'User'
+
+  has_many :workers, :class_name => 'User', :through => :ticket_assignments
+
+  has_many :activities
+
+  has_many :comments
+  has_many :status_changes
+  has_and_belongs_to_many :labels
+
+  def recent_activities(limit)
+    activities.order('created_at DESC').limit(limit)
+  end
+
+  def status
+    status_changes.last
+  end
+end
+
+class Activity < ActiveRecord::Base
+  belongs_to :subject, :polymorphic => true
+  belongs_to :user
+end
+
+class Label < ActiveRecord::Base
+  belongs_to :project
+  has_and_belongs_to_many :tickets
+end
+
+class Comment < ActiveRecord::Base
+  belongs_to :ticket
+  belongs_to :user
+
+  after_create :create_activities
+
+  def boss
+    ticket.owner
+  end
+
+  private
+
+  def create_activities
+    [boss, user].uniq.each do |person|
+    #[boss].push(user).uniq.each do |person|
+      Activity.create(
+        subject: self,
+        name: 'comment_posted',
+        ticket: ticket,
+        user: person
+      )
+    end
+  end
+end
+
+class StatusChange < ActiveRecord::Base
+  belongs_to :ticket
+  belongs_to :user
+
+  after_create :create_activities
+
+  def is_open?
+    # switch statement determining open/closed status based on string name
+    false
+  end
+
+  private
+
+  def create_activities
+    Activity.create(
+      subject: self,
+      name: 'status_changed',
+      ticket: ticket,
+      user: user
+    )
+  end
+end
+
+class ProjectAssignment < ActiveRecord::Base
+  belongs_to :project
+  belongs_to :user
+end
+
+class TicketAssignment < ActiveRecord::Base
+  belongs_to :ticket
+  belongs_to :user
+end
+
+
+# ==================================================
+
 
 class SLSProject
   include DataMapper::Resource
@@ -89,7 +241,7 @@ class SLSLabel
   property :name, String
   property :sls_id, Integer
   property :github_id, Integer
-  
+
   belongs_to :project, 'SLSProject'
   has n, :tickets, :through => :labelings
 end
@@ -133,5 +285,3 @@ class SLSTicket
 
   #attr_accessor :project, :assigned_to, :labels, :title, :priority, :status, :sls_id, :comments
 end
-
-
