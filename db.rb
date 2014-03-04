fd7b442fd383d533dc3ffea4da8c0df08c30535d
David Alexander <david@campusspecial.com>
2014-03-04 17:54:59 -0500

    Some polymorphic fields were backwards. Opening the way for Labels to come in

--- db.rb
+++ db.rb
@@ -7,284 +7,217 @@ require 'sqlite3'
 require 'active_record'
 require 'yaml'
 
-dbconfig = YAML::load(File.open('database.yml'))
-ActiveRecord::Base.establish_connection(dbconfig)
+module SLS
+  dbconfig = YAML::load(File.open('database.yml'))
+  ActiveRecord::Base.establish_connection(dbconfig)
 
-ActiveRecord::Schema.define do
-  unless ActiveRecord::Base.connection.tables.include? 'users'
-    create_table :users do |table|
-      table.column :f_name, :string
-      table.column :l_name, :string
-      table.column :email, :string
-      table.column :short_name, :string
-      table.column :sls_id, :integer
-      table.column :github_id, :integer
+  def self.import_ticket_nums(project_name, filename)
+    proj = SLS::Project.where(:name => project_name).first
+    File.readlines(filename).each do |line|
+      num = line.chomp.scan(/^(\d+),/)
+      unless num.empty?
+        SLS::Ticket.find_or_create_by(
+          :sls_id => num[0][0].to_i,
+          :project_id => proj.id
+        )
+      end
+    end
+  end
+  
+  ActiveRecord::Schema.define do
+    unless ActiveRecord::Base.connection.tables.include? 'tickets'
+      create_table :tickets do |table|
+        table.string :title
+        table.text :body
+        table.string :priority, :default => 'NORMAL'
+        table.integer :sls_id
+        table.integer :github_id
+        table.datetime :created
+
+        # Foreign keys
+        table.integer :project_id # project
+        table.integer :owner_id # user
+      end
+    end
+
+    unless ActiveRecord::Base.connection.tables.include? 'users'
+      create_table :users  do |table|
+        table.string :f_name
+        table.string :l_name
+        table.string :email
+        table.string :short_name
+        table.integer :sls_id
+        table.integer :github_id
+      end
+    end
+
+    unless ActiveRecord::Base.connection.tables.include? 'projects'
+      create_table :projects do |table|
+        table.string :name
+        table.integer :sls_id
+        table.integer :github_id
+      end
+    end
+
+    unless ActiveRecord::Base.connection.tables.include? 'assignments'
+      # Many-to-many facilitator
+      create_table :assignments do |table|
+        table.integer :subject_id, :null => false
+        table.string :subject_type, :null => false
+        table.integer :assignable_id, :null => false
+        table.string :assignable_type, :null => false
+      end
+    end
+
+    unless ActiveRecord::Base.connection.tables.include? 'activities'
+      # Many-to-many facilitator
+      create_table :activities do |table|
+        table.integer :subject_id, :null => false
+        table.string :subject_type, :null => false
+        table.integer :activity_id, :null => false
+        table.string :activity_type, :null => false
+      end
+    end
+
+    unless ActiveRecord::Base.connection.tables.include? 'comments'
+      create_table :comments do |table|
+        table.text :body, :null => false
+        table.integer :author_id, :null => false
+        table.datetime :created
+        table.integer :ticket_id, :null => false
+        table.integer :sls_id
+      end
+    end
+
+    unless ActiveRecord::Base.connection.tables.include? 'status_changes'
+      create_table :status_changes do |table|
+        table.boolean :open, :default => true, :null => false
+        table.string :status
+        table.integer :ticket_id, :null => false
+        table.integer :author, :null => false
+      end
+    end
+
+    unless ActiveRecord::Base.connection.tables.include? 'labels'
+      create_table :labels do |table|
+        table.string :name, :null => false
+        table.integer :project_id, :null => false
+        table.integer :github_id
+        table.integer :sls_id
+      end
     end
   end
 
-  unless ActiveRecord::Base.connection.tables.include? 'projects'
-    create_table :projects do |table|
-      table.column :name, :string
-      table.column :sls_id, :integer
-      table.column :github_id, :integer
+
+  class Ticket < ActiveRecord::Base
+    belongs_to :project, :class_name => 'SLS::Project'
+    belongs_to :owner, :class_name => 'SLS::User'
+
+    # Assignments
+    has_many :assignments, :class_name => 'SLS::Assignment', :as => :subject
+    has_many :workers, :class_name => 'SLS::User', :through => :assignments,
+      :as => :assignable, :source => :assignable, :source_type => 'SLS::User'
+    has_many :labels, :class_name => 'SLS::Label', :through => :assignments,
+      :as => :assignable, :source => :assignable, :source_type => 'SLS::Label'
+
+    # Activities
+    has_many :activities, :class_name => 'SLS::Activity', :as => :subject
+    has_many :status_changes, :class_name => 'SLS::StatusChange',
+      :through => :activities, :as => :activity, :source => :subject,
+      :source_type => 'SLS::Ticket'
+    has_many :comments, :class_name => 'SLS::Comment',
+      :through => :activities, :as => :activity, :source => :subject,
+      :source_type => 'SLS::Ticket'
+
+    def add_worker(user)
+      self.assign user if user.is_a? SLS::User and not self.workers.include? user
+    end
+
+    def add_label(label)
+      self.assign label if label.is_a? SLS::Label and not self.labels.include? label
+    end
+
+    def add_comment(body, author, sls_id=nil, created=DateTime.now)
+      SLS::Comment.create(
+        :body => body,
+        :author => author,
+        :ticket => self,
+        :sls_id => sls_id,
+        :created => created
+      ) if author.is_a?(SLS::User) && created.is_a?(DateTime)
+    end
+
+    def assign(tag)
+      SLS::Assignment.create(:subject => self, :assignable => tag)
+      self.reload
     end
   end
 
-  unless ActiveRecord::Base.connection.tables.include? 'projectassignments'
-    create_table :projectassignments do |table|
-      # many-to-many facilitator
-      table.column :project_id, :integer
-      table.column :user_id, :integer
-    end
+  class Project < ActiveRecord::Base
+    has_many :tickets, :class_name => 'SLS::Ticket'
   end
 
-  unless ActiveRecord::Base.conneciton.tables.include? 'tickets'
-    create_table :tickets do |table|
-      table.column :title, :string
-      table.column :body, :text
-      table.column :priority, :string
-      table.column :is_open, :boolean
-      table.column :sls_id, :integer
-      table.column :github_id, :integer
-      table.column :created, :timestamp
-      
-      # Foreign Keys
-      table.column :owner, :integer
-      table.column :project_id, :integer
-    end
+  class User < ActiveRecord::Base
+    has_many :creations, :class_name => 'SLS::Ticket'
+    has_many :assignments, :class_name => 'SLS::Assignment', :as => :subject
+    has_many :tasks, :source => :assignable, :source_type => 'SLS::Ticket',
+      :class_name => 'SLS::Ticket', :through => :assignments
+    has_many :comments, :class_name => 'SLS::Comment', :foreign_key => :author_id
   end
 
-  unless ActiveRecord::Base.connection.tables.include? 'ticketassignments'
-    create_table :ticketassignments do |table|
-      # many-to-many facilitator
-      table.column :user, :integer
-      table.column :ticket, :integer
-    end
-  end
+  class Comment < ActiveRecord::Base
+    belongs_to :ticket, :class_name => 'SLS::Ticket'
+    belongs_to :author, :class_name => 'SLS::User'
+    after_create :create_activities
 
-  unless ActiveRecord::Base.connection.tables.include? 'labels'
-    create_table :labels do |table|
-      table.column :name, :string
-      
-      # Foreign keys
-      table.column :project_id, :integer
-    end
-  end
+    private
 
-  unless ActiveRecord::Base.connection.tables.include? 'labelings'
-    create_table :labelings do |table|
-      # many-to-many facilitator
-      table.column :label, :integer
-      table.column :ticket, :integer
-    end
-  end
-
-  unless ActiveRecord::Base.connection.tables.include? 'ticketevents'
-    create_table :ticketevents do |table|
-      table.column :created, :timestamp
-      table.column :table, :string
-      table.column :table_id, :integer
-
-      # Foreign keys
-      table.column :ticket_id, :integer
-    end
-  end
-
-  unless ActiveRecord::Base.connection.tables.include? 'comments'
-    create_table :comments do |table|
-      table.column :comment, :text
-      table.column :sls_id, :integer
-      table.column :github_id, :integer
-      table.column :created, :timestamp
-
-      # Foreign keys
-      table.column :owner, :integer # user account
-      table.column :ticketevent_id, :integer
-    end
-  end
-
-  unless ActiveRecord::Base.connection.tables.include? 'statuschanges'
-    create_table :statuschanges do |table|
-      table.column :name, :string
-      table.column :is_open, :boolean
-      table.column :created, :timestamp
-
-      # Foreign keys
-      table.column :ticketevent_id, :integer
-    end
-  end
-end
-
-class User < ActiveRecord::Base
-  has_many :creations, :class_name => 'Ticket'
-  has_many :tasks, :class_name => 'Ticket', :through => :ticket_assignments
-  has_many :projects, :through => :project_assignments
-  has_many :comments
-  has_many :activities
-
-  def recent_activities(limit)
-    activities.order('created_at DESC').limit(limit)
-  end
-end
-
-class Project < ActiveRecord::Base
-  has_many :tickets
-  has_many :members, :class_name => 'User', :through => :project_assigments
-  has_many :activities, :through => :tickets
-end
-
-class Ticket < ActiveRecord::Base
-  belongs_to :project
-  belongs_to :creator, :class_name => 'User'
-
-  has_many :workers, :class_name => 'User', :through => :ticket_assignments
-
-  has_many :activities
-
-  has_many :comments
-  has_many :status_changes
-  has_and_belongs_to_many :labels
-
-  def recent_activities(limit)
-    activities.order('created_at DESC').limit(limit)
-  end
-
-  def status
-    status_changes.last
-  end
-end
-
-class Activity < ActiveRecord::Base
-  belongs_to :subject, :polymorphic => true
-  belongs_to :user
-end
-
-class Label < ActiveRecord::Base
-  belongs_to :project
-  has_and_belongs_to_many :tickets
-end
-
-class Comment < ActiveRecord::Base
-  belongs_to :ticket
-  belongs_to :user
-
-  after_create :create_activities
-
-  def boss
-    ticket.owner
-  end
-
-  private
-
-  def create_activities
-    [boss, user].uniq.each do |person|
-    #[boss].push(user).uniq.each do |person|
-      Activity.create(
-        subject: self,
-        name: 'comment_posted',
-        ticket: ticket,
-        user: person
+    def create_activities
+      SLS::Activity.create(
+        :activity => self,
+        :subject => self.ticket
       )
     end
   end
-end
 
-class StatusChange < ActiveRecord::Base
-  belongs_to :ticket
-  belongs_to :user
+  class StatusChange < ActiveRecord::Base
+    belongs_to :ticket, :class_name => 'SLS::Ticket'
+    belongs_to :author, :class_name => 'SLS::User'
+    after_create :create_activities
 
-  after_create :create_activities
+    private
 
-  def is_open?
-    # switch statement determining open/closed status based on string name
-    false
+    def create_activities
+      SLS::Activity.create(
+        :activity => self,
+        :subject => self.ticket
+      )
+    end
   end
 
-  private
+  class Label < ActiveRecord::Base
+    belongs_to :project, :class_name => 'SLS::Project'
+    has_many :assignments, :class_name => 'SLS::Assignment', :as => :assignable
+    has_many :tickets, :class_name => 'SLS::Ticket', :through => :assignments,
+      :as => :subject, :source => :assignable, :source_type => 'SLS::Label'
 
-  def create_activities
-    Activity.create(
-      subject: self,
-      name: 'status_changed',
-      ticket: ticket,
-      user: user
-    )
+    def add_to_ticket(ticket)
+      #
+    end
+
+    def assign(tag)
+      SLS::Assignment.create(:subject => self, :assignable => tag)
+      self.reload
+    end
   end
-end
 
-class ProjectAssignment < ActiveRecord::Base
-  belongs_to :project
-  belongs_to :user
-end
+  class Assignment < ActiveRecord::Base
+    belongs_to :subject, :polymorphic => true
+    belongs_to :assignable, :polymorphic => true
+  end
 
-class TicketAssignment < ActiveRecord::Base
-  belongs_to :ticket
-  belongs_to :user
-end
-
-
-# ==================================================
-
-
-class SLSProject
-  include DataMapper::Resource
-
-  property :id, Serial
-  property :name, String
-  property :sls_id, Integer
-  property :github_id, Integer
-
-  has n, :tickets, 'SLSTicket'
-  has n, :labels, 'SLSLabel'
-end
-
-class SLSLabel
-  include DataMapper::Resource
-
-  property :name, String
-  property :sls_id, Integer
-  property :github_id, Integer
-
-  belongs_to :project, 'SLSProject'
-  has n, :tickets, :through => :labelings
-end
-
-class SLSLabeling
-  include DataMapper::Resource
-
-  belongs_to :label, 'SLSLabel'
-  belongs_to :ticket, 'SLSTicket'
-end
-
-class SLSStatusChange
-  include DataMapper::Resource
-
-  property :name, String
-  property :is_open, Boolean
-  property :created, DateTime, :default => DateTime.now
-end
-
-class SLSComment
-  include DataMapper::Resource
-
-  property :comment, Text
-  property :created, DateTime, :default => DateTime.now
-  property :sls_id, Integer
-  property :github_id, Integer
-end
-
-class SLSTicket
-  include DataMapper::Resource
-
-  property :title, String
-  property :body, Text
-  property :priority, String
-  property :is_open, Boolean
-  property :sls_id, Integer
-  property :github_id, Integer
-
-  belongs_to :owner, 'SLSUser'
-  belongs_to :project, 'SLSProject'
-
-  #attr_accessor :project, :assigned_to, :labels, :title, :priority, :status, :sls_id, :comments
+  class Activity < ActiveRecord::Base
+    belongs_to :subject, :polymorphic => true
+    belongs_to :activity, :polymorphic => true
+  end
 end
